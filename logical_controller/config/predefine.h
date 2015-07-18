c7b76ff9dd0a5b0cb23db2feefd1675ae58a6fd3
lionxing <xingjian.xj@alibaba-inc.com>
2015-07-18 17:46:33 +0800

    add new file

---
+++ logical_controller/config/predefine.h
@@ -0,0 +1,13 @@
+//
+//  predefine.h
+//  logical_controller
+//
+//  Created by lionxing on 18/7/15.
+//  Copyright © 2015年 lionxing. All rights reserved.
+//
+
+#ifndef predefine_h
+#define predefine_h
+
+
+#endif /* predefine_h */
