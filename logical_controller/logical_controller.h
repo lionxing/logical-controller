d35497c45bdc0bd5478442f42375c952ed1eac32
lionxing <xingjian.xj@alibaba-inc.com>
2015-07-18 16:59:31 +0800

    this a new day, this a new world~

---
+++ logical_controller/logical_controller.h
@@ -0,0 +1,13 @@
+//
+//  logical_controller.h
+//  logical_controller
+//
+//  Created by lionxing on 18/7/15.
+//  Copyright © 2015年 lionxing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface logical_controller : NSObject
+
+@end
---
+++ logical_controller/logical_controller.m
@@ -0,0 +1,13 @@
+//
+//  logical_controller.m
+//  logical_controller
+//
+//  Created by lionxing on 18/7/15.
+//  Copyright © 2015年 lionxing. All rights reserved.
+//
+
+#import "logical_controller.h"
+
+@implementation logical_controller
+
+@end
